5f229e74acc7ae477e74b0730dfac59b1514b63a
Vipin Kumar <vipin.kumar03@motherson.com>
2025-04-21 16:30:06 +0530

    Leet code questions solved.

--- DSA/src/datastructures/LinkedList.java
+++ DSA/src/datastructures/LinkedList.java
@@ -64,39 +64,158 @@ public class LinkedList {
 	 * Remove last item from linked list
 	 */
 	public Node removeLast() {
-		if(length == 0) return null;
+		if (length == 0)
+			return null;
 		Node temp = head;
 		Node pre = head;
-		while(temp.next != null) {
+		while (temp.next != null) {
 			pre = temp;
 			temp = temp.next;
 		}
 		tail = pre;
 		tail.next = null;
 		length--;
-		if(length == 0) {
+		if (length == 0) {
 			head = null;
 			tail = null;
 		}
-		
+
 		return temp;
 	}
-	
+
 	/**
-	 * Appends the item at the start of linked list 
+	 * Appends the item at the start of linked list
 	 */
 	public Node prependNode(int value) {
 		Node newNode = new Node(value);
-		if(length == 0) {
+		if (length == 0) {
 			head = newNode;
 			tail = newNode;
-		}else {
-			newNode.next=head;
-			head=newNode;
+		} else {
+			newNode.next = head;
+			head = newNode;
 		}
 		length++;
-		
+
 		return head;
+	}
+
+	/**
+	 * Remove item from the start of linked list
+	 */
+	public Node removeFirst() {
+		Node first = head;
+		if (length == 0)
+			return null;
+		head = head.next;
+		first.next = null;
+		length--;
+		if (length == 0)
+			tail = null;
+
+		return first;
+
+	}
+
+	public Node getNode(int index) {
+		if (index < 0 || index >= length)
+			return null;
+		Node temp = head;
+		for (int i = 0; i < index; i++) {
+			temp = temp.next;
+		}
+		return temp;
+	}
+
+	public boolean setNode(int index, int value) {
+		Node node = getNode(index);
+		if (node != null) {
+			node.value = value;
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean insert(int index, int value) {
+		if (index < 0 || index > length)
+			return false;
+		if (index == 0) {
+			prependNode(value);
+			length++;
+			return true;
+		}
+		if (index == length) {
+			append(value);
+			length++;
+			return true;
+		}
+		Node node = new Node(value);
+		Node temp = getNode(index - 1);
+		node.next = temp.next;
+		temp.next = node;
+		length++;
+		return true;
+	}
+
+	public Node removeItem(int index) {
+		if (index < 0 || index >= length)
+			return null;
+		if (index == 0)
+			return removeFirst();
+		if (index == length - 1)
+			return removeLast();
+		Node node = getNode(index - 1);
+		Node temp = node.next;
+		node.next = temp.next;
+		temp.next = null;
+		length--;
+		return temp;
+	}
+
+	public void reverse() {
+		Node temp = head;
+		head = tail;
+		tail = temp;
+		Node after = null;
+		Node before = null;
+		for (int i = 0; i < length; i++) {
+			after = temp.next;
+			temp.next = before;
+			before = temp;
+			temp = after;
+		}
+	}
+
+	public Node findMiddleNode() {
+		if (head == null)
+			return null;
+
+		Node slow = head;
+		Node fast = head;
+
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+
+		return slow;
+	}
+
+	public boolean hasLoop() {
+		if (head == null)
+			return false;
+
+		Node slow = head;
+		Node fast = head;
+
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast)
+				return true;
+		}
+		return false;
 	}
 
 }
--- DSA/src/datastructures/Main.java
+++ DSA/src/datastructures/Main.java
@@ -5,23 +5,40 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		LinkedList linkedList = new LinkedList(4);
+		LinkedList linkedList = new LinkedList(1);
+		linkedList.append(2);
+		linkedList.append(3);
+		linkedList.append(4);
 		linkedList.append(5);
-		linkedList.append(6);
-		linkedList.append(7);
+//		linkedList.append(8);
+		System.out.print("\n================ Initials =======================\n");
 		System.out.println("Head: "+linkedList.getHead().value);
 		System.out.println("Tail: "+linkedList.getTail().value);
 		System.out.println("Length: "+linkedList.getLength());
 		Node temp=linkedList.getHead();
-		System.out.print("Linked List ");
+		System.out.print("Original Linked List ");
 		while(temp!=null) {
 			System.out.print(temp.value+" ");
 			temp=temp.next;
 		}
+
+		System.out.print("\n================ Operation =======================");
 //		System.out.print("\nremoved " + linkedList.removeLast().value);
-		System.out.print("\nPrepended " + linkedList.prependNode(3).value);
+//		System.out.print("\nPrepended " + linkedList.prependNode(3).value);
+//		System.out.print("\nRemoved First: " + linkedList.removeFirst().value);
+//		System.out.print("\nGet Node: " + linkedList.getNode(3).value);
+//		System.out.print("\nSet Node: " + linkedList.setNode(3,8));
+//		System.out.print("\nInsert : " + linkedList.insert(1,8));
+//		System.out.print("\nRemove from index : " + linkedList.removeItem(3));
+//		System.out.print("\nReverse : ");linkedList.reverse();
+//		System.out.print("\nMiddle Node : "+linkedList.findMiddleNode().value);
+		System.out.print("\nHas loop : "+linkedList.hasLoop());
 		
-		System.out.print("\nUpdated Linked List ");
+		
+		System.out.println("\n=================================================\nUpdated Head: "+linkedList.getHead().value);
+		System.out.println("Updated Tail: "+linkedList.getTail().value);
+		System.out.println("Updated Length: "+linkedList.getLength());
+		System.out.print("\nUpdated Linked List: ");
 		Node temp1=linkedList.getHead();
 		while(temp1!=null) {
 			System.out.print(temp1.value+" ");
